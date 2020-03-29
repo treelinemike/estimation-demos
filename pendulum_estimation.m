902328882d8590c1413aa19b67d6b536b38a1d46
Mike Kokko <32647624+treelinemike@users.noreply.github.com>
2020-03-29 13:58:48 -0400

    added initial sampling, etc. committing before breaking out state propagation into three separate ODE calls.

--- pendulum_estimation.m
+++ pendulum_estimation.m
@@ -1,8 +1,3 @@
-% NOTE: CONSIDER REMOVING LATEX INTERPRETER IN LEGENDS B/C MAC AND SOME
-% OLDER VERSIONS OF MATLAB THROW ERRORS WITH THAT
-
-% plot phase portraits for the simple pendulum
-
 % restart
 close all; clear all; clc;
 
@@ -11,20 +6,21 @@ close all; clear all; clc;
 rng(2374,'twister');
 
 % simulation time parameters
-SIGMA_w_true = [0.01^2 0; 0 0.0875^2];   % covariance matrix for state propigation noise (note: rows correspond to errors in DERIVATIVES of state variables)
+SIGMA_w_true = [0.01^2 0; 0 0.0875^2];   % covariance matrix for state propagation noise (note: rows correspond to errors in DERIVATIVES of state variables)
 t0 = 0;       % [s] simulation start time
 tf = 20;      % [s] simulation end time
 dt = 0.1;     % [s] timestep size
 opts = odeset('RelTol',1e-8,'AbsTol',1e-12);
 
 % sampling options
-sigma_v = 0.002;    % standard deviation of sensor noise; assumed to be well known (truth value = value used in estimator)
+SIGMA_v = 0.002;    % standard deviation of sensor noise; assumed to be well known (truth value = value used in estimator)
 
 % estimator options
-SIGMA_w = [0.002^2 0; 0 0.1^2];   % assumed covariance matrix for state propigation noise (note: rows correspond to errors in DERIVATIVES of state variables)
+Np = 2000;                       % number of particles
+SIGMA_w = [0.02^2 0; 0 0.1^2];   % assumed covariance matrix for state propagation noise (note: rows correspond to errors in DERIVATIVES of state variables)
 
-
-% define parameters for cases to analyze
+% define parameters of physical system in a structure
+% that we can pass through the ODE solver to the update function
 sysParams = [];
 sysParams.m = 2;   
 sysParams.l = 1;
@@ -32,12 +28,12 @@ sysParams.c = 1;
 sysParams.g = 9.81;
 sysParams.SIGMA_w_true = SIGMA_w_true;
 
-% initial conditions (state vector: [theta theta_dot]')
+% initial conditions (state vector: [theta theta_dot]' stacked as: [stochastic truth; undamped model propagation; deterministic truth])
 theta_t_0     = 25*pi/180;      % [rad]
 theta_t_dot_0 = 0;              % [rad/s]
 theta_m_0     = 25*pi/180;      % [rad]
 theta_m_dot_0 = 0;              % [rad/s]
-X0 = [theta_t_0 theta_t_dot_0 theta_m_0 theta_m_dot_0 theta_t_0 theta_t_dot_0]';  % [rad rad/s rad rad/s]'
+X0 = [theta_t_0 theta_t_dot_0 theta_m_0 theta_m_dot_0 theta_t_0 theta_t_dot_0]';  % [rad rad/s rad rad/s rad rad/s]'
 X = X0;
 
 % data storage
@@ -52,12 +48,12 @@ for t = t0:dt:(tf-dt)
     odeTime = [t t+dt];
     
     % generate the random noise for this timestep
-    % generate state propigation noise
+    % generate state propagation noise
     % want to keep the random draw CONSTANT inside of the ODE45 update
     % function, so draw it here and pass it in
     sysParams.w_t = mvnrnd([0 0]',SIGMA_w_true,1)';
 
-    % propigate state
+    % propagate state
     [T,X] = ode45(@(t,X) propDynamics(t,X,sysParams),odeTime,X,opts);
     X = X(end, :)';  % note: this step is necessary to keep state vector dimensions correct for next call to ode45()
     
@@ -98,13 +94,18 @@ linkaxes(ax,'x');
 
 % sample the true signal
 dt_samp = 0.25;  % observation sampling period
-t_samp = 0:dt_samp:time(end);
-z_samp = interp1(time,z_true,t_samp)' + sigma_v*randn(length(t_samp),1);
+t_samp = dt_samp:dt_samp:time(end);  % NOTE: DO NOT sample at the initial state (we assume that the initial state estimate is given/known)
+z_samp = interp1(time,z_true,t_samp)' + SIGMA_v*randn(length(t_samp),1);
 plot(time,z_true,'Color',[0 0.7 0],'LineWidth',1.6);
 plot(t_samp,z_samp,'.','MarkerSize',20,'Color','m');
 
-%%
+%% show angular position, velocity, and acceleration for the pendulum
+% these are the TRUTH traces
+% one is the deterministic solution
+% the other is the stochastic solution which we will take as acutal truth
+% for the estimator
 figure;
+set(gcf,'Position',[0697 0122 0550 0822]);
 ax2 = subplot(3,1,1);
 hold on; grid on;
 
@@ -174,10 +175,42 @@ xlim([0,max(time)]);
 % we will step through time and filter to the current timestep
 
 % first, draw a sample of particles from the initial state
-% Sigma1 = diag(
-% Xp1 = X0
+mu = data(1:2,1);
+Xp = mvnrnd(mu',SIGMA_w,Np)';
+x_true = data(:,1);
 
-for tidx = 1:length(time)
+% initialize figure
+figure; 
+
+% step through time
+% evaluating one "frame" at a time moving from
+% local state 1 (beginning of frame) to local state 2 (end of frame)
+% note that the first observation is NOT at the initial time b/c we assume
+% that we have an initial state estimate
+for obsIdx = 1:length(t_samp)
+    
+    % show particles at start of the estimation frame (state 1)
+    subplot(1,3,1);
+    hold on; grid on;
+    plot(Xp(1,:),Xp(2,:),'.','MarkerSize',5,'Color',[.6 .6 1]);
+    plot(mu(1),mu(2),'bo','MarkerSize',10,'LineWidth',3);
+    plot(x_true(1),x_true(2),'o','MarkerSize',20,'Color',[0 0.5 0],'LineWidth',3);
+    plot(data(1,:),data(2,:),'.-','Color',[0 0.5 0],'LineWidth',1); % true trajectory in state space
+    xlabel('\bfx_1: Angular Position [rad]');
+    ylabel('\bfx_2: Angular Velocity [rad/s]');
+
+    % now propigate and update each particle
+    for particleIdx = 1:Np
+        
+        % get previous posterior particle set
+        x_prev = Xp(:,particleIdx);
+        
+        % compute "proposal distribution" by propagating particles forward
+        % in time through system model
+        x_prior = 
+        
+        
+    end
     
 end
 
@@ -204,10 +237,17 @@ theta_t_dot_det = X(6);
 % note:     X    = [theta_t      theta_t_dot   theta_m      theta_m_dot]
 % therefore Xdot = [theta_t_dot  theta_t_ddot  theta_m_dot  theta_m_ddot] + w_t
 Xdot = zeros(6,1);
+
+% stochastic truth
 Xdot(1,:) = theta_t_dot + w_t(1);
 Xdot(2,:) = -(c/(m*l^2))*theta_t_dot-1*(g/l)*sin(theta_t) + w_t(2);
-Xdot(3,:) = theta_m_dot + w_t(1);
-Xdot(4,:) = -1*(9.81/(1.1*l))*sin(theta_m) + w_t(2);
+
+% undamped model propagation
+% does not 
+Xdot(3,:) = theta_m_dot;
+Xdot(4,:) = -1*(9.81/(1.1*l))*sin(theta_m);
+
+% deterministic truth
 Xdot(5,:) = theta_t_dot_det;
 Xdot(6,:) = -(c/(m*l^2))*theta_t_dot_det-1*(g/l)*sin(theta_t_det);
 end
