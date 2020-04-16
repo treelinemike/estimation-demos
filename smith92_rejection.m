ae6661163a77f6ee44741926e3c680185b15d193
Mike Kokko <32647624+treelinemike@users.noreply.github.com>
2020-04-15 22:02:49 -0400

    got both smith92 methods working

--- smith92_rejection.m
+++ smith92_rejection.m
@@ -1,3 +1,7 @@
+% rejection method of sampling from Smith & Gelfand 1992
+% have a pdf g(x) and a function f(x) which could (but need not) be normalized to a pdf h(x)
+% sample from g(x), then transform those samples so that they could have been generated from h(x)
+
 % restart
 close all; clear all; clc;
 rng(182,'twister');
@@ -8,18 +12,19 @@ sigma_g = 1;
 mu_h = 5;
 sigma_h = 0.5;
 
-% number of samples
-N = 50000
+% sample size
+N = 50000;
 
 % constant factor
 M = 30; 
 
 % domain
-x = 0:0.001:10;
+x = -1:0.001:8;
 
+% functions g(x) and h(x)... sampling from g(x), transforming to be samples from h(x)
 g = (1/(sigma_g*sqrt(2*pi)))*exp(-0.5*((x-mu_g)/sigma_g).^2);
-h = (1/(sigma_h*sqrt(2*pi)))*exp(-0.5*((x-mu_h)/sigma_h).^2);
-
+h = (1/(sigma_h*sqrt(2*pi)))*exp(-0.5*((x-mu_h)/sigma_h).^2);  % note: f(x) is NOT normalized...
+f = exp(-0.5*((x-mu_h)/sigma_h).^2);  % note: f(x) is NOT normalized...
 
 % sample from g(x)
 samp_g = mu_g + randn(N,1)*sigma_g;
@@ -34,23 +39,29 @@ u = rand(size(samp_g));
 samp_h = [];
 for sampIdx = 1:length(samp_g)
    thisXIdx = find( x >= samp_g(sampIdx),1,'first');
-   if( u(sampIdx) <=  h(thisXIdx)/(g(thisXIdx)*M) )
+   if( u(sampIdx) <=  f(thisXIdx)/(g(thisXIdx)*M) )
        samp_h(end+1) = samp_g(sampIdx);
    end
 end
 
+% smooth accepted samples
+h_ks = ksdensity(samp_h,x);%,'Kernel','Epanechnikov');
 
 % plot results
 figure;
+set(gcf,'Position',[0085 0309 1.220800e+03 0420]);
 hold on; grid on;
-plot(x,g,'--','LineWidth',1.6,'Color',[0 0.6 0]);
-plot(x,h,'--','LineWidth',1.6,'Color',[0.6 0 0]);
-plot(samp_g,zeros(size(samp_g)),'.','MarkerSize',10,'Color',[0 1 0]);
-plot(x,g_ks,'LineWidth',1.6,'Color',[0 1 0]);
+plot(x,g,'-','LineWidth',1.6,'Color',[0.8 0 0]);
+plot(x,f,'-','LineWidth',1.6,'Color',[0 0.8 0]);
+plot(x,h,'--','LineWidth',1.6,'Color',[0 0.8 0]);
+% plot(x,g_ks,':','LineWidth',1.6,'Color',[0.8 0 0]);
 % plot(x,h./g,'m-','LineWidth',1.6);
-plot(samp_h,zeros(size(samp_h)),'.','MarkerSize',5,'Color',[1 0 0]);
-h_ks = ksdensity(samp_h,x,'Kernel','normal');%'Epanechnikov');
-plot(x,h_ks,'LineWidth',1.6,'Color',[1 0 0]);
+plot(x,h_ks,'LineWidth',1.6,'Color',[0 0 0.8]);
+plot(samp_g,zeros(size(samp_g)),'.','MarkerSize',10,'Color',[0.8 0 0]);
+plot(samp_h,zeros(size(samp_h)),'o','MarkerSize',5,'Color',[0 0 0.8]);
+legend('g(x)','f(x)','h(x)','Accepted PDF','g(x) Samples','Accepted','Location','NorthWest');
+xlim([-1 8]);
+title('\bfRejection Method');
 
 % show stats 
 N_exp = ceil((1/M)*trapz(x,h)*N);
--- smith92_weighted_bootstrap.m
+++ smith92_weighted_bootstrap.m
@@ -1,6 +1,14 @@
+% weighted bootstrap method of sampling from Smith & Gelfand 1992
+% have a pdf g(x) and a function f(x) which could (but need not) be normalized to a pdf h(x)
+% sample from g(x), then transform those samples so that they could have been generated from h(x)
+
 % restart
 close all; clear all; clc;
 rng(182,'twister');
+
+% options
+doSortParticlesBeforeResamplilng = 0;
+doUseStandardBootstrap = 0;
 
 % means and standard deviations of two distributions
 mu_g = 3;
@@ -8,65 +16,74 @@ sigma_g = 1;
 mu_h = 5;
 sigma_h = 0.5;
 
-% number of samples
-N = 1000
-
+% sample sizes
+N_g = 1000;   % number of samples of g(x)
+N_rs = 1000;  % number of points to resample
 
 % domain
-x = 0:0.001:10;
+x = -1:0.001:8;
 
+% functions g(x), h(x), and f(x)... sampling from g(x), transforming via f(x) to be samples from h(x)
 g = (1/(sigma_g*sqrt(2*pi)))*exp(-0.5*((x-mu_g)/sigma_g).^2);
-f = exp(-0.5*((x-mu_h)/sigma_h).^2);
+h = (1/(sigma_h*sqrt(2*pi)))*exp(-0.5*((x-mu_h)/sigma_h).^2);  % note: f(x) is NOT normalized...
+f = exp(-0.5*((x-mu_h)/sigma_h).^2);  % note: f(x) is NOT normalized...
 
 % sample from g(x)
-samp_g = mu_g + randn(N,1)*sigma_g;
+samp_g = mu_g + randn(N_g,1)*sigma_g;
 
 % smooth sample from g(x)
 g_ks = ksdensity(samp_g,x,'Kernel','normal');%'Epanechnikov');
 
-
-% compute weights
+% find indices of samples in the x vector
 samp_g_idx = arrayfun(@(thisSamp) find(x >= thisSamp,1,'first'),samp_g); 
-w = f(samp_g_idx)./g(samp_g_idx);
-q = w/sum(w);
-
-
-figure;
-hold on; grid on;
-plot(x,g,'-','Color',[0.8 0 0],'LineWidth',1.6);
-plot(x,f,'-','Color',[0 0.8 0],'LineWidth',1.6);
-plot(samp_g,zeros(size(samp_g)),'.','MarkerSize',15);
-plot(x(samp_g_idx),zeros(size(samp_g_idx)),'o','MarkerSize',10,'Color',[0 0 0.8],'LineWidth',2);
-plot(x(samp_g_idx),q,'m.','MarkerSize',10);
-
-error('asdf');
-%%%
-% sample from uniform distribution
-u = rand(size(samp_g));
-
-% accept/reject samples
-samp_h = [];
-for sampIdx = 1:length(samp_g)
-   thisXIdx = find( x >= samp_g(sampIdx),1,'first');
-   if( u(sampIdx) <=  f(thisXIdx)/(g(thisXIdx)*M) )
-       samp_h(end+1) = samp_g(sampIdx);
-   end
+if( doSortParticlesBeforeResamplilng)
+    samp_g_idx = sort(samp_g_idx);
 end
 
+% compute weights
+if(doUseStandardBootstrap)
+    q = 1/N_g*ones(1,N_g);  % standard bootstrap uses a uniform PMF
+else
+    w = f(samp_g_idx)./g(samp_g_idx); % weight each sample by the ratio of f(x)/g(x) at the appropriate x
+    q = w/sum(w); % this is now a PMF (not a PDF)
+end
+q_cdf = cumsum(q); % the samp_g_idx vector maps these particles back to the samples of g
+
+% resample using inverse transform sampling (i.e. universaility of the uniform)
+u = rand(N_rs,1);
+rs_sampnums = arrayfun(@(x) find(q_cdf >= x,1,'first'),u);  % sampled particle numbers, not indexed to samples of g(x)
+resamp_x = x(samp_g_idx( rs_sampnums ));
+resamp_ks = ksdensity(resamp_x,x); %,'Kernel','Epanechnikov');
 
 % plot results
 figure;
+set(gcf,'Position',[1.540000e+01 4.586000e+02 1.495200e+03 2.888000e+02]);
+subplot(1,3,1);
 hold on; grid on;
-plot(x,g,'--','LineWidth',1.6,'Color',[0 0.6 0]);
-plot(x,f,'--','LineWidth',1.6,'Color',[0.6 0 0]);
-plot(samp_g,zeros(size(samp_g)),'.','MarkerSize',10,'Color',[0 1 0]);
-plot(x,g_ks,'LineWidth',1.6,'Color',[0 1 0]);
-% plot(x,h./g,'m-','LineWidth',1.6);
-plot(samp_h,zeros(size(samp_h)),'.','MarkerSize',5,'Color',[1 0 0]);
-h_ks = ksdensity(samp_h,x,'Kernel','normal');%'Epanechnikov');
-plot(x,h_ks,'LineWidth',1.6,'Color',[1 0 0]);
+plot(x,g,'-','Color',[0.8 0 0],'LineWidth',1.6);
+plot(x,f,'-','Color',[0 0.8 0],'LineWidth',1.6);
+plot(x,h,'--','Color',[0 0.8 0],'LineWidth',1.6);
+plot(x,resamp_ks,'-','Color',[0 0 0.8],'LineWidth',1.6);
+plot(x(samp_g_idx),zeros(1,N_g),'.','Color',[0.8 0 0],'MarkerSize',10);
+plot(resamp_x,zeros(1,N_rs),'o','Color',[0 0 0.8],'MarkerSize',5);
+legend('g(x)','f(x)','h(x)','Resample PDF','g(x) Samples','Resample','Location','NorthWest');
+xlim([-1 8]);
+title('\bfWeighted Bootstrap');
+    
+subplot(1,3,2);
+hold on; grid on;
+stem(1:length(q),q,'-','MarkerSize',1,'Color',[0.8 0 0]);
+xlabel('\bfOriginal Sample/Particle Index');
+ylabel('\bfProbability');
+title('\bfSample PMF');
 
-% show stats 
-N_exp = ceil((1/M)*trapz(x,f)*N);
-fprintf('%d prior particles downsampled to %d posterior particles (expected %d).\n',length(samp_g),length(samp_h),N_exp);
- 
+subplot(1,3,3);
+hold on; grid on;
+plot(1:length(q),q_cdf,'-','Color',[0.8 0 0]);
+plot(zeros(size(u)),u,'.','MarkerSize',10,'Color',[0 0 0.8]);
+plot( rs_sampnums , zeros(size(rs_sampnums)),'.','MarkerSize',10,'Color',[0.8 0 0.8]);
+xlabel('\bfOriginal Sample/Particle Index');
+ylabel('\bfProbability');
+title('\bfSample CDF and Resample');
+ylim([0 1]);
+legend('CDF','Uniform Samples','Samples from PMF/CDF','Location','NorthWest');
