b2faf26e9e7a775cd01bf88901862d9a2b35de1d
Mike Kokko <32647624+treelinemike@users.noreply.github.com>
2020-04-23 13:55:01 -0400

    reworked gibbs test to be extensible to >2 dimensions

--- gibbs_2d_test.m
+++ gibbs_2d_test.m
@@ -1,4 +1,5 @@
-% 2D gibbs sampling test
+% Gibbs Sampler: Test in 2D
+% Core algorithm
 
 % restart
 close all; clear; clc;
@@ -9,22 +10,23 @@ rng('default');
 % options
 Nsteps = 100;
 doShowSteps = 0;
+doAnimate = 0;
 
 % true PDF parameters
-mu_true = [2 5]';
+mu_true = [2 5];
 sigma_1 = 2;
 sigma_2 = 4;
 corr = 0.8;
 cov_true = [sigma_1^2 corr*sigma_1*sigma_2; corr*sigma_1*sigma_2 sigma_2^2];
 
 % state space to explore (i.e. query points)
-x1q_vec = -3:0.1:7;
-x2q_vec = -4:0.1:13;
+xq_vec{1} = (-3:0.1:7)';
+xq_vec{2} = (-4:0.1:13)';
 
 % generate PDF at query points
-[X1Q,X2Q] = meshgrid(x1q_vec,x2q_vec);
-XQ = [X1Q(:), X2Q(:)];
-[PQ] = mvnpdf(XQ,mu_true',cov_true);
+[X1Q,X2Q] = meshgrid(xq_vec{1},xq_vec{2});
+XQ = [X1Q(:) X2Q(:)];
+[PQ] = mvnpdf(XQ,mu_true,cov_true);
 PDF = reshape(PQ,size(X1Q));
 
 % plot true PDF
@@ -52,72 +54,59 @@ plot(x0(1),x0(2),'ko','MarkerSize',10,'LineWidth',2.0);
 for gibbsIter = 1:Nsteps
     for dimIdx = 1:length(mu_true)
         
-        switch dimIdx
-            case 1
-                % slice PDF along this dimension at the current point
-                gibbsx1 = x1q_vec;
-                gibbsx2 = x(2)*ones(size(x1q_vec));
-                gibbsXQ = [gibbsx1' gibbsx2'];
-                gibbsPQ = mvnpdf(gibbsXQ,mu_true',cov_true);
-                gibbsPQ = gibbsPQ/trapz(gibbsx1,gibbsPQ);
-                
-                % compute CDF and sample from it using inverse transform sampling
-                gibbsCDF = cumtrapz(gibbsx1,gibbsPQ);
-                gibbsPt = gibbsx1(arrayfun( @(x) find(x <= gibbsCDF,1,'first') , rand(1,1) ));
-                x(dimIdx) = gibbsPt;
-                gibbsSamp = gibbsx1(arrayfun( @(x) find(x <= gibbsCDF,1,'first') , rand(1,100) ));  % for illustration only
-                
-                if(doShowSteps)
-                    figure(1);
-                    plot(gibbsx1,gibbsx2,'r-','LineWidth',1.0);
-                    plot(x(1),x(2),'r+','MarkerSize',10,'LineWidth',3);
+        % reset storage
+        gibbsXQ = repmat(x',length(xq_vec{dimIdx}),1);
+        gibbsXQ(:,dimIdx) = xq_vec{dimIdx};
+        
+        % slice PDF along this dimension at the current point
+        gibbsPQ = mvnpdf(gibbsXQ,mu_true,cov_true);
+        gibbsPQ = gibbsPQ/trapz(gibbsXQ(:,dimIdx),gibbsPQ);
+        
+        % compute CDF and sample from it using inverse transform sampling
+        gibbsCDF = cumtrapz(gibbsXQ(:,dimIdx),gibbsPQ);
+        gibbsPt = gibbsXQ( arrayfun( @(x) find(x <= gibbsCDF,1,'first') , rand(1,1)) ,dimIdx);
+        x(dimIdx) = gibbsPt;
+        gibbsSamp = gibbsXQ( arrayfun( @(x) find(x <= gibbsCDF,1,'first') , rand(1,100)) ,dimIdx);  % for illustration only
+        
+        % plot results of stepping along this dimension if requested
+        if(doShowSteps)
+            figure(1);
+            plot(x(1),x(2),'r+','MarkerSize',10,'LineWidth',3);  % selected point
+            switch(dimIdx)
+                case 1
+                    plot(gibbsXQ(:,dimIdx),x(2)*ones(size(gibbsXQ,1),1),'r-','LineWidth',1.0);
                     plot(gibbsSamp,x(2)*ones(size(gibbsSamp)),'r.','MarkerSize',10);  % for illustration only
-                    
-                    figure;
-                    hold on; grid on;
-                    plot(gibbsx1,gibbsPQ);
-                    plot(gibbsSamp,0*ones(size(gibbsSamp)),'r.','MarkerSize',10);
-                    plot(gibbsPt,0,'r+','MarkerSize',10,'LineWidth',3);
-                end
-                
-            case 2
-                % slice PDF along this dimension at the current point
-                gibbsx1 = x(1)*ones(size(x2q_vec));
-                gibbsx2 = x2q_vec;;
-                gibbsXQ = [gibbsx1' gibbsx2'];
-                gibbsPQ = mvnpdf(gibbsXQ,mu_true',cov_true);
-                gibbsPQ = gibbsPQ/trapz(gibbsx2,gibbsPQ);
-                
-                % compute CDF and sample from it using inverse transform sampling
-                gibbsCDF = cumtrapz(gibbsx2,gibbsPQ);
-                gibbsPt = gibbsx2(arrayfun( @(x) find(x <= gibbsCDF,1,'first') , rand(1,1) ));
-                x(dimIdx) = gibbsPt;
-                gibbsSamp = gibbsx2(arrayfun( @(x) find(x <= gibbsCDF,1,'first') , rand(1,100) ));  % for illustration only
-                
-                if(doShowSteps)
-                    figure(1);
-                    plot(gibbsx1,gibbsx2,'b-','LineWidth',1.0);
-                    plot(x(1),x(2),'b+','MarkerSize',10,'LineWidth',3);
-                    plot(x(1)*ones(size(gibbsSamp)),gibbsSamp,'b.','MarkerSize',10);  % for illustration only
-                    
-                    figure;
-                    hold on; grid on;
-                    plot(gibbsx2,gibbsPQ);
-                    plot(gibbsSamp,0*ones(size(gibbsSamp)),'b.','MarkerSize',10);
-                    plot(gibbsPt,0,'b+','MarkerSize',10,'LineWidth',3);
-                end
-                
+                case 2
+                    plot(x(1)*ones(size(gibbsXQ,1),1),gibbsXQ(:,dimIdx),'r-','LineWidth',1.0);
+                    plot(x(1)*ones(size(gibbsSamp)),gibbsSamp,'r.','MarkerSize',10);  % for illustration only
+            end
+            
+            % show 1-D plot
+            figure;
+            hold on; grid on;
+            plot(gibbsXQ(:,dimIdx),gibbsPQ);
+            plot(gibbsSamp,0*ones(size(gibbsSamp)),'r.','MarkerSize',10);
+            plot(gibbsPt,0,'r+','MarkerSize',10,'LineWidth',3);
         end
         
-        
     end
+    
+    % store new point in markov chain after moving in each direction
     x_hist(:,gibbsIter+1) = x;
-    figure(1);
-    plot(x(1),x(2),'k.','MarkerSize',10);
-    plot(x(1),x(2),'ko','MarkerSize',10,'LineWidth',2.0);
-    plot(x_hist(1,:),x_hist(2,:),'k-');
+    
+    % show result of this iteration if requested
+    if(doAnimate)
+        figure(1);
+        plot(x(1),x(2),'k.','MarkerSize',10);
+        plot(x(1),x(2),'ko','MarkerSize',10,'LineWidth',2.0);
+        plot(x_hist(1,:),x_hist(2,:),'k.-','MarkerSize',10);
+        pause(0.1);
+    end
     
 end
 
-
-
+% show all points in the markov chain
+figure(1);
+plot(x_hist(1,:),x_hist(2,:),'k.-','MarkerSize',10);
+plot(x_hist(1,:),x_hist(2,:),'k.','MarkerSize',10);
+plot(x_hist(1,:),x_hist(2,:),'ko','MarkerSize',10,'LineWidth',2.0);
