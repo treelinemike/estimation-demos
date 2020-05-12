9d6164fd000fb6c917ecf10f12cee607c15f7bf3
Mike Kokko <32647624+treelinemike@users.noreply.github.com>
2020-05-12 06:30:13 -0400

    added switch to allow display, more work to do.

--- gibbs_nd_test.m
+++ gibbs_nd_test.m
@@ -8,6 +8,7 @@ close all; clear; clc;
 rng('default');
 
 % options
+doUseExternalGibbsFcn = 0;
 doOneStepOnly = 0;
 doShowSteps = 0;
 doAnimate = 0;
@@ -118,117 +119,123 @@ cmap = colormap;
 set(gca,'Color',cmap(1,:));
 set(gca,'GridAlpha',0.6);
 
-x_samp_post = gibbsSampleRect(xq_cp, ks_pdf, dimLengths, Np, gibbsBurnIn, gibbsM);
+if(doUseExternalGibbsFcn)
+    x_samp_post = gibbsSampleRect(xq_cp, ks_pdf, dimLengths, Np, gibbsBurnIn, gibbsM);
+else
+    % start sampler at random query point
+    x0Idx = randi(size(xq_cp,2));
+    x0 = xq_cp(:,x0Idx);
+    % plot(x1(1),x0(2),'.','Color',[0 0.8 0],'MarkerSize',20);
+    
+    % compute number of steps to take
+    if(doOneStepOnly)
+        Nsteps = 1;
+    else
+        Nsteps = gibbsBurnIn + 1 + gibbsM*(Np-1);
+    end
+    
+    % start history
+    x = x0;
+    xIdx = x0Idx;
+    x_hist = NaN(length(mu_true),Nsteps+1);
+    x_hist(:,1) = x0;
+    xIdx_hist = NaN(1,Nsteps+1);
+    xIdx_hist(1) = x0Idx;
+    
+    % show starting point
+    if(doShowSteps)
+        plot3(x0(1),x0(2),2,'o','MarkerSize',5,'LineWidth',2,'Color',[0 0.8 0]);
+        zlim([0 3]);
+    end
+    
+    % iterate sampler
+    for gibbsIter = 1:Nsteps
+        for dimIdx = 1:nDim
+            
+            % extract indices for all points in a line along the selected
+            % dimension that includes the current point
+            subscripts = ndind2sub(dimLengths,xIdx);
+            pointSubscripts = repmat(subscripts,length(xq_vec{dimIdx}),1);
+            pointSubscripts(:,dimIdx) = (1:length(xq_vec{dimIdx}))';
+            ind = ndsub2ind(dimLengths,pointSubscripts);
+            
+            % compute 1D PDF and CDF along this line
+            pointsAlongDim = xq_cp(:,ind);
+            distAlongDim = pointsAlongDim - pointsAlongDim(:,1);
+            distAlongDim = vecnorm(distAlongDim,2,1);
+            normFactor = trapz(distAlongDim,ks_pdf(ind));
+            pdf = ks_pdf(ind)/normFactor;
+            cdf = cumtrapz(distAlongDim,pdf);
+            
+            % sample a point from the CDF
+            localIdx = find(cdf >= rand(1),1,'first');
+            xIdx = ind(localIdx);
+            x = xq_cp(:,xIdx);
+            
+            % plot results of stepping along this dimension if requested
+            if(doShowSteps)
+                figure(mainFigIdx);
+                subplot(1,2,2);
+                plot3(pointsAlongDim(1,:),pointsAlongDim(2,:),2*ones(size(pointsAlongDim,2)),'k.','MarkerSize',5);
+                plot3(xq_cp(1,xIdx),xq_cp(2,xIdx),2,'ro','MarkerSize',5,'LineWidth',2);
+                zlim([0 3]);
+                
+                % sample many points just to observe distribution
+                multiSampleLocalIdx = arrayfun(@(x) find(cdf >= x,1,'first'),rand(1,100));
+                
+                % compute true PDF along this line
+                trueNormFactor = trapz(distAlongDim,PQ(ind));
+                truePDF = PQ(ind)/trueNormFactor;
+                
+                % show 1-D plot
+                figure;
+                hold on; grid on;
+                plot(distAlongDim,pdf,'-','LineWidth',1.6,'Color',[0 0 0.8]);
+                plot(distAlongDim,truePDF,'--','LineWidth',1.6,'Color',[0 0 0.8]);
+                plot(distAlongDim(multiSampleLocalIdx),0*ones(size(multiSampleLocalIdx)),'r.','MarkerSize',10);
+                plot(distAlongDim(localIdx),0,'r+','MarkerSize',10,'LineWidth',3);
+                legend('Emperical Density','True Density','Samples','Chosen Sample');
+                title(sprintf('True Norm Factor: %0.4f',trueNormFactor));
+            end
+        end
+        
+        % after stepping once along each eigendirection,
+        % store new point as next node in markov chain
+        x_hist(:,gibbsIter+1) = x;
+        xIdx_hist(gibbsIter+1) = xIdx;
+        
+        % show result of this iteration if requested
+        if(doAnimate)
+            figure(mainFigIdx);
+            plot(x(1),x(2),'k.','MarkerSize',10);
+            plot(x(1),x(2),'ko','MarkerSize',10,'LineWidth',2.0);
+            plot(x_hist(1,:),x_hist(2,:),'k.-','MarkerSize',10);
+            
+            if(doMakeVideo)
+                thisImgFile = sprintf('frame%03d.png',gibbsIter);
+                saveas(gcf,thisImgFile);
+                system(['convert -trim ' thisImgFile ' ' thisImgFile]);  % REQUIRES convert FROM IMAGEMAGICK!
+            else
+                pause(0.1);
+            end
+        end
+        
+    end
+    
 
-% % % start sampler at random query point
-% % x0Idx = randi(size(xq_cp,2));
-% % x0 = xq_cp(:,x0Idx);
-% % % plot(x1(1),x0(2),'.','Color',[0 0.8 0],'MarkerSize',20);
-% % 
-% % % compute number of steps to take
-% % if(doOneStepOnly)
-% %     Nsteps = 1;
-% % else
-% %    Nsteps = gibbsBurnIn + 1 + gibbsM*(Np-1); 
-% % end
-% % 
-% % % start history
-% % x = x0;
-% % xIdx = x0Idx;
-% % x_hist = NaN(length(mu_true),Nsteps+1);
-% % x_hist(:,1) = x0;
-% % xIdx_hist = NaN(1,Nsteps+1);
-% % xIdx_hist(1) = x0Idx;
-% % 
-% % % show starting point
-% % if(doShowSteps)
-% %     plot3(x0(1),x0(2),2,'o','MarkerSize',5,'LineWidth',2,'Color',[0 0.8 0]);
-% %     zlim([0 3]);
-% % end
-% % 
-% % % iterate sampler
-% % for gibbsIter = 1:Nsteps
-% %     for dimIdx = 1:nDim
-% %         
-% %         % extract indices for all points in a line along the selected
-% %         % dimension that includes the current point
-% %         subscripts = ndind2sub(dimLengths,xIdx);
-% %         pointSubscripts = repmat(subscripts,length(xq_vec{dimIdx}),1);
-% %         pointSubscripts(:,dimIdx) = (1:length(xq_vec{dimIdx}))';
-% %         ind = ndsub2ind(dimLengths,pointSubscripts);
-% %         
-% %         % compute 1D PDF and CDF along this line
-% %         pointsAlongDim = xq_cp(:,ind);
-% %         distAlongDim = pointsAlongDim - pointsAlongDim(:,1);
-% %         distAlongDim = vecnorm(distAlongDim,2,1);
-% %         normFactor = trapz(distAlongDim,ks_pdf(ind));
-% %         pdf = ks_pdf(ind)/normFactor;
-% %         cdf = cumtrapz(distAlongDim,pdf);
-% %         
-% %         % sample a point from the CDF
-% %         localIdx = find(cdf >= rand(1),1,'first');
-% %         xIdx = ind(localIdx);
-% %         x = xq_cp(:,xIdx);
-% %        
-% %         % plot results of stepping along this dimension if requested
-% %         if(doShowSteps)
-% %             figure(mainFigIdx);
-% %             subplot(1,2,2);
-% %             plot3(pointsAlongDim(1,:),pointsAlongDim(2,:),2*ones(size(pointsAlongDim,2)),'k.','MarkerSize',5);
-% %             plot3(xq_cp(1,xIdx),xq_cp(2,xIdx),2,'ro','MarkerSize',5,'LineWidth',2);
-% %             zlim([0 3]);
-% %             
-% %             % sample many points just to observe distribution
-% %             multiSampleLocalIdx = arrayfun(@(x) find(cdf >= x,1,'first'),rand(1,100));
-% % 
-% %             % compute true PDF along this line
-% %             trueNormFactor = trapz(distAlongDim,PQ(ind));
-% %             truePDF = PQ(ind)/trueNormFactor;
-% % 
-% %             % show 1-D plot
-% %             figure;
-% %             hold on; grid on;
-% %             plot(distAlongDim,pdf,'-','LineWidth',1.6,'Color',[0 0 0.8]);
-% %             plot(distAlongDim,truePDF,'--','LineWidth',1.6,'Color',[0 0 0.8]);
-% %             plot(distAlongDim(multiSampleLocalIdx),0*ones(size(multiSampleLocalIdx)),'r.','MarkerSize',10);
-% %             plot(distAlongDim(localIdx),0,'r+','MarkerSize',10,'LineWidth',3);
-% %             legend('Emperical Density','True Density','Samples','Chosen Sample');
-% %             title(sprintf('True Norm Factor: %0.4f',trueNormFactor));
-% %         end
-% %     end
-% %     
-% %     % after stepping once along each eigendirection,
-% %     % store new point as next node in markov chain
-% %     x_hist(:,gibbsIter+1) = x;
-% %     xIdx_hist(gibbsIter+1) = xIdx;
-% %     
-% %     % show result of this iteration if requested
-% %     if(doAnimate)
-% %         figure(mainFigIdx);
-% %         plot(x(1),x(2),'k.','MarkerSize',10);
-% %         plot(x(1),x(2),'ko','MarkerSize',10,'LineWidth',2.0);
-% %         plot(x_hist(1,:),x_hist(2,:),'k.-','MarkerSize',10);
-% %         
-% %         if(doMakeVideo)
-% %             thisImgFile = sprintf('frame%03d.png',gibbsIter);
-% %             saveas(gcf,thisImgFile);
-% %             system(['convert -trim ' thisImgFile ' ' thisImgFile]);  % REQUIRES convert FROM IMAGEMAGICK!
-% %         else
-% %             pause(0.1);
-% %         end
-% %     end
-% %     
-% % end
+    % downsample to keep only selected points from the markov chain
+    x_samp_post = x_hist(:,((gibbsBurnIn+1):gibbsM:Nsteps));
+end
 
-% keep only selected points from the markov chain
-% figure(mainFigIdx);
-% subplot(1,2,2);
-% hold on; grid on;
+% show sampled point cloud
+figure(mainFigIdx);
+subplot(1,2,2);
+hold on; grid on;
 plot3(x_samp_post(1,:),x_samp_post(2,:),ones(size(x_samp_post,2)),'.','MarkerSize',4,'Color',[ 0 0 0 ]);
 
 % principal axes of regularized particle set
 mu = mean(x_samp_post,2);
-    cov = 1/(size(x_samp_post,2)-1)*(x_samp_post-mu)*(x_samp_post-mu)';
+cov = 1/(size(x_samp_post,2)-1)*(x_samp_post-mu)*(x_samp_post-mu)';
 [vec,val] = eig(cov);
 acos(vec(1,1))*180/pi
 % choose sign of eigenvectors s.t. rotated basis is within +/- 90deg of
